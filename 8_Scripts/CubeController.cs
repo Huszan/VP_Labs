ed0914f2f73bc6997d444449f68280add9d4f6ff
Huszan <mateuszjacenty1@gmail.com>
2020-05-16 15:43:23 +0200

    ini

---
+++ 8_Scripts/CubeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public float speed = 1f;
+    public enum ChooseVector { X,Y,Z}
+    public ChooseVector chooseVector;
+
+    void Update()
+    {
+        if (chooseVector == ChooseVector.X) this.transform.Rotate(speed, 0, 0, Space.Self);
+        else if (chooseVector == ChooseVector.Y) this.transform.Rotate(0, speed, 0, Space.Self);
+        else if (chooseVector == ChooseVector.Z) this.transform.Rotate(0, 0, speed, Space.Self);
+    }
+}
---
+++ 8_Scripts/s1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour
+{
+    private GameObject[] go;
+    void Start()
+    {
+        go = GameObject.FindGameObjectsWithTag("kula");
+        for(int i = 0; i < go.Length; i++)
+        {
+            Destroy(go[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
