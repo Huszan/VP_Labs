a80e9bcf20be3ae1b61b708f8021c418315b9378
Huszan <mateuszjacenty1@gmail.com>
2020-05-23 17:46:58 +0200

    Ini

---
+++ Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Material[] mat;
+    public MeshRenderer mr;
+
+    private void Start()
+    {
+        mr.material = mat[Random.Range(0, mat.Length)];
+    }
+    void Update()
+    {
+        float transX = Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+        float transY = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+
+        if (Input.GetMouseButton(0))
+        {
+            transform.Translate(transX, transY, 0);
+        }
+    }
+}
--- Scripts/spawner.cs
+++ Scripts/spawner.cs
@@ -2,42 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawner : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
-    private bool canSpawn = true;
-    private float timer;
+    public GameObject toSpawn;
 
-    public GameObject[] objectList;
-    public float interval = 3f;
-
-    private void spawn(int number)
+    private void spawn()
     {
-        Instantiate(objectList[number], this.transform);
+        Instantiate(toSpawn);
     }
-
     void Update()
     {
-        if (timer >= 0.0f && canSpawn)
+        if (Input.GetMouseButtonDown(1))
         {
-            timer += Time.deltaTime;
+            spawn();
         }
-        if (timer >= interval)
-        {
-            spawn(Random.Range(0, objectList.Length - 1));
-            timer = 0.0f;
-        }
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            if (canSpawn) canSpawn = false;
-            else canSpawn = true;
-            Debug.Log("Can spawn = " + canSpawn);
-        }
-        if (Input.GetKeyDown(KeyCode.Space)) spawn(Random.Range(0, objectList.Length - 1));
-        if (Input.GetKeyDown(KeyCode.Alpha1)) spawn(0);
-        if (Input.GetKeyDown(KeyCode.Alpha2)) spawn(1);
-        if (Input.GetKeyDown(KeyCode.Alpha3)) spawn(2);
-        if (Input.GetKeyDown(KeyCode.Alpha4)) spawn(3);
-        if (Input.GetKeyDown(KeyCode.Alpha5)) spawn(4);
     }
-
 }
